0fdb8ea6bd955261b6a44a3543d7c64577b81f90
Denis Istomin <rex.hd.kanal@gmail.com>
2024-07-11 10:05:59 +0300

    app_test fixture

--- tests/app_test.cpp
+++ tests/app_test.cpp
@@ -6,7 +6,35 @@
 #include <QTest>
 #include <QTimer>
 using namespace testing;
-TEST(app_test, simpleTest)
+int _argc;
+char** _argv;
+class AppFixture : public Test
 {
-	EXPECT_EQ(1000 - 7, 993);
+protected:
+	void SetUp() override
+	{
+		app = new App(_argc, _argv);
+		window = app->engine()->rootObjects().isEmpty() ? nullptr 
+			: (QQuickWindow*)app->engine()->rootObjects().first();
+	}
+	void TearDown() override
+	{
+		QTimer exitTimer;
+		exitTimer.setSingleShot(true);
+		QObject::connect(&exitTimer, &QTimer::timeout, [=]() {app->exit(0); });
+		exitTimer.start(100);
+		app->exec();
+		delete app;
+	}
+	App* app;
+	QQuickWindow* window;
+};
+TEST(app_test, app_creation)
+{
+	ASSERT_NO_THROW(App(_argc,_argv));
+}
+int main(int argc, char** argv) {
+	_argc = argc; _argv = argv;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
