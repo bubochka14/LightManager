70d8767b1f6d1c9fee304f8bc0fb773333492f22
Denis Istomin <rex.hd.kanal@gmail.com>
2024-07-18 12:43:09 +0300

    added logic to application methods; added connectionTimeout property

--- app/app.cpp
+++ app/app.cpp
@@ -2,43 +2,85 @@
 using namespace Qt::Literals::StringLiterals;
 Q_LOGGING_CATEGORY(LC_APP, "Application");
 App::App(int argc, char **argv)
-
+    :_qApp(argc,argv)
+    ,_serial(new SerialTransport(this))
+    ,_disp(new MessageDispatcher(this))
+    ,_sett("Org"_L1,"Light Manager"_L1)
+    ,_engine(new QQmlApplicationEngine)
 {
+    qCDebug(LC_APP) << "creation ...";
+    _engine->setInitialProperties({
+        {"portNames",_serial->availablePorts()} });
+    _engine->load(QUrl("qrc:/Main.qml"_L1));
+    _view  = _engine->rootObjects().isEmpty()?nullptr:_engine->rootObjects().first();
+    if(!_view)
+    {
+        qCCritical(LC_APP)<< "window creation error.";
+    }
+    else
+    {
+        connect(_view, SIGNAL(connectToPort(const QString&)),
+            this, SLOT(handleConnect(const QString&)));
+        qCDebug(LC_APP) << "created.";
+    }
+}
+void App::handleConnect(const QString& port)
+{
+    _serial->openPort(port);
+    _view->setProperty("pageState", "connecting");
 
 }
-
+quint32 App::connectionTimeout() const
+{
+    return _timeout;
+}
+void App::setConnectionTimeout(quint32 other)
+{
+    if (other == _timeout)
+        return;
+    _timeout = other;
+    emit connectionTimeoutChanged();
+}
 QQmlApplicationEngine * App::engine() const
 {
-    return nullptr;
+    return _engine;
 }
 
 int App::exec()
 {
-    return 0;
+    if (!_view)
+        return -1;
+    qCDebug(LC_APP) << "executed.";
+    return _qApp.exec();
 }
 void App::exit(int code)
 {
-
+    qCDebug(LC_APP) << "closed.";
+    _qApp.exit(code);
 }
 void App::setSerialTransport(SerialTransport* other)
 {
-   
+    if (other == _serial)
+        return;
+    _serial = other;
+    _view->setProperty("portNames", _serial->availablePorts());
+    emit serialTransportChanged();
 }
 App::~App()
 {
 }
-
-
 SerialTransport* App::serialTransport() const
 {
-    return nullptr;
+    return _serial;
 }
-
 void App::setMessageDispatcher(MessageDispatcher* other)
 {
-
+    if (_disp == other)
+        return;
+    _disp = other;
+    emit messageDispatcherChanged();
 }
 MessageDispatcher * App::messageDispatcher() const
 {
-    return nullptr;
+    return _disp;
 }
--- app/app.h
+++ app/app.h
@@ -2,7 +2,9 @@
 #define APP_H
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QQmlComponent>
+#include <QQuickItem>
+#include <QSettings>
 #include "serialtransport.h"
 #include "messagedispatcher.h"
 #include "qloggingcategory.h"
@@ -10,17 +12,36 @@ Q_DECLARE_LOGGING_CATEGORY(LC_APP);
 class App : public QObject
 {
     Q_OBJECT;
+    Q_PROPERTY(quint32 connectionTimeout READ connectionTimeout WRITE setConnectionTimeout NOTIFY connectionTimeoutChanged);
+    Q_PROPERTY(SerialTransport* serialTransport READ serialTransport WRITE setSerialTransport NOTIFY serialTransportChanged);
+    Q_PROPERTY(MessageDispatcher* messageDispatcher READ messageDispatcher WRITE setMessageDispatcher NOTIFY messageDispatcherChanged);
 public:
     explicit App(int argc, char **argv);
     int exec();
     void exit(int code=0);
 public:
     QQmlApplicationEngine * engine() const;
+    quint32 connectionTimeout() const;
+    void setConnectionTimeout(quint32 other);
     void setSerialTransport(SerialTransport* other);
     SerialTransport* serialTransport() const;
     void setMessageDispatcher(MessageDispatcher* other);
     MessageDispatcher* messageDispatcher() const;
     virtual ~App();
+signals:
+    void connectionTimeoutChanged();
+    void serialTransportChanged();
+    void messageDispatcherChanged();
+protected slots:
+    void handleConnect(const QString& port);
+private:
+    QGuiApplication _qApp;
+    QQmlApplicationEngine* _engine;
+    SerialTransport* _serial;
+    MessageDispatcher* _disp;
+    QObject * _view;
+    quint32 _timeout;
+    QSettings _sett;
 };
 
 #endif // APP_H
