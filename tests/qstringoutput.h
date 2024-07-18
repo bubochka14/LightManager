4fa79584f7b926aeafe5f15590d492f0b41030c7
Denis Istomin <rex.hd.kanal@gmail.com>
2024-07-18 13:06:21 +0300

    make QString PrintTo func inline

--- tests/qstringoutput.h
+++ tests/qstringoutput.h
@@ -3,7 +3,7 @@
 #include <iostream>
 //This header must be included in all sources where gtest print with QString is used to avoid ODR violations
 QT_BEGIN_NAMESPACE
-void PrintTo(const QString& qString, ::std::ostream* os)
+inline void PrintTo(const QString& qString, ::std::ostream* os)
 {
     *os << qPrintable(qString);
 }
